ef55acc6a03fc5fca21412eea2edfcb3c8be3be4
FedeNicoletti <federiconicoletti@outlook.com>
2021-02-06 05:35:56 -0300

    Update scoreboard

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const userScore = 0;
-const computerScore = 0;
-const userScore_div = document.getElementById("userscore");
-const computerScore_div = document.getElementById("computerscore");
+let userScore = 0;
+let computerScore = 0;
+const userScore_p = document.getElementById("userscore");
+const computerScore_p = document.getElementById("computerscore");
 const rock_button = document.getElementById("rock");
 const paper_button = document.getElementById("paper");
 const scissors_button = document.getElementById("scissors");
@@ -17,21 +17,39 @@ function gamePlay(userSelection){
         return;
     } if (userSelection == "Rock" && computerChoice == "Paper"){
         winner.textContent = "Seriously? Computer choose Paper... TRY AGAIN!"
+        computerScore++;
+        userScore_p.textContent = userScore;
+        computerScore_p.textContent = computerScore;
         return;
     } if (userSelection == "Scissors" && computerChoice == "Rock"){
         winner.textContent = "Come on! Computer choose Rock... TRY AGAIN!"
+        computerScore++;
+        userScore_p.textContent = userScore;
+        computerScore_p.textContent = computerScore;
         return;
     } if (userSelection == "Paper" && computerChoice == "Scissors"){
         winner.textContent = "Oh my god... Computer choose Scissors... TRY AGAIN!"
+        computerScore++;
+        userScore_p.textContent = userScore;
+        computerScore_p.textContent = computerScore;
         return;
     } if (userSelection == "Rock" && computerChoice == "Scissors"){
         winner.textContent = "AMAZING! Computer choose Scissors and you win!"
+        userScore++;
+        userScore_p.textContent = userScore;
+        computerScore_p.textContent = computerScore;
         return;
     } if (userSelection == "Scissors" && computerChoice == "Paper"){
         winner.textContent = "INCREDIBLE! Computer choose Paper and you win!"
+        userScore++;
+        userScore_p.textContent = userScore;
+        computerScore_p.textContent = computerScore;
         return;
     } if (userSelection == "Paper" && computerChoice == "Rock"){
         winner.textContent = "OK! Computer choose Rock and you win!"
+        userScore++;
+        userScore_p.textContent = userScore;
+        computerScore_p.textContent = computerScore;
         return;
     }  
 }
