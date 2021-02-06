cbcc25a910f473adbf8c742cc2bc3ae27bd01433
FedeNicoletti <federiconicoletti@outlook.com>
2021-02-06 05:16:05 -0300

    Update comparisors

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-function playRound(playerSelection, computerSelection){
-   
-    if(playerSelection == computerSelection){
-        return ("It's a tie!");
-    } if (playerSelection == "Rock" && computerSelection == "Paper"){
-        return ("Computer choose Paper and wins... Try again!" )
-    } if (playerSelection == "Scissors" && computerSelection == "Rock"){
-        return ("Computer choose Rock and wins... Try again!" )
-    } if (playerSelection == "Paper" && computerSelection == "Scissors"){
-        return ("Computer choose Scissors and wins... Try again!" )
-    } if (playerSelection == "Rock" && computerSelection == "Siccors"){
-        return ("Congrats! Computer choose Scissors and you wins!" )
-    } if (playerSelection == "Scissors" && computerSelection == "Paper"){
-        return ("Congrats! Computer choose Paper and you wins!" )
-    } if (playerSelection == "Paper" && computerSelection == "Rock"){
-        return ("Congrats! Computer choose Rock and you wins!" )
-    }
+const userScore = 0;
+const computerScore = 0;
+const userScore_div = document.getElementById("userscore");
+const computerScore_div = document.getElementById("computerscore");
+const rock_button = document.getElementById("rock");
+const paper_button = document.getElementById("paper");
+const scissors_button = document.getElementById("scissors");
+
+function gamePlay(userSelection){
+    const computerChoice = computerSelection();
+    
+    //Update text
+   const winner = document.querySelector('.winner');
+   //Check selections
+    if(userSelection == computerChoice){
+        winner.textContent = "DRAWWWWWWW!";
+        return;
+    } if (userSelection == "Rock" && computerChoice == "Paper"){
+        winner.textContent = "Seriously? Computer choose Paper... TRY AGAIN!"
+        return;
+    } if (userSelection == "Scissors" && computerChoice == "Rock"){
+        winner.textContent = "Come on! Computer choose Rock... TRY AGAIN!"
+        return;
+    } if (userSelection == "Paper" && computerChoice == "Scissors"){
+        winner.textContent = "Oh my god... Computer choose Scissors... TRY AGAIN!"
+        return;
+    } if (userSelection == "Rock" && computerChoice == "Scissors"){
+        winner.textContent = "AMAZING! Computer choose Scissors and you win!"
+        return;
+    } if (userSelection == "Scissors" && computerChoice == "Paper"){
+        winner.textContent = "INCREDIBLE! Computer choose Paper and you win!"
+        return;
+    } if (userSelection == "Paper" && computerChoice == "Rock"){
+        winner.textContent = "OK! Computer choose Rock and you win!"
+        return;
+    }  
 }
 
 function playerSelection(){
-    let options = ["Rock", "Paper", "Scissors"];
-        let random = Math.floor(Math.random()*(options.length));
-        let selection = options[random];
-        return(selection);
+   
+    rock_button.addEventListener("click", function(){
+     gamePlay("Rock");
+   })
+    paper_button.addEventListener("click", function(){
+   gamePlay("Paper");
+   })
+    scissors_button.addEventListener("click", function(){
+   gamePlay("Scissors");
+   })    
 }
 
 function computerSelection(){
         let options = ["Rock", "Paper", "Scissors"];
         let random = Math.floor(Math.random()*(options.length));
-        let selection = options[random];
-        return(selection);
+        return options[random];
 }
 
-const playerPlay =  playerSelection();
-const computerPlay = computerSelection();
-console.log(playRound(playerPlay, computerPlay));
+gamePlay();
+playerSelection();
+computerSelection();
+
